b649fb9b819771758b2157586fcf7e376b1dfb39
Abdurrahman Rajab <rajab-2@windowslive.com>
2024-04-14 20:45:09 +0300

    feat: add sales api

---
+++ reverse-engineering-lab/src/app/api/sales/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest } from "next/server";
+export async function GET(request: NextRequest) {
+    const data = [
+        {name: 'Joe', surname:"Jack", sales: 2400, email: "joe@randomemail.com"},
+        {name: 'John', surname:"Doe", sales: 4560, email: "john@randomemail.com"},
+        {name: 'Jane', surname:"Doe", sales: 1390, email: "jame@randomemail.com"},
+        {name: 'Janice', surname:"Doe", sales: 980, email: "janice@randomemail.com"},
+    ];
+    return new Response(JSON.stringify({ data }), {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+}
