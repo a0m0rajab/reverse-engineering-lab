26aed2f268771c2bc188262553b9cc47e73aaf85
Abdurrahman Rajab <rajab-2@windowslive.com>
2024-04-27 15:20:47 +0300

    feat: add middleware to check autherization

---
+++ reverse-engineering-lab/src/middleware.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+function isPublic(path: string) {
+    // console.log(path)
+    let publicUrls = ['/login', "/"];
+    let isPublic = publicUrls.some((p) =>{
+        console.log("isPublic ", p, path, path === p);
+        return path === p;
+    });
+    return isPublic;
+};
+
+export function middleware(request: NextRequest) {
+  // Assume a "Cookie:nextjs=fast" header to be present on the incoming request
+  // Getting cookies from the request using the `RequestCookies` API
+  let isLoggedIn = request.cookies.get('loggedIn')?.value || false;
+//   console.log('isLoggedIn:', isLoggedIn)
+  console.log('request.url:', request.nextUrl.pathname)
+  console.log(isPublic(request.nextUrl.pathname));
+  if (isPublic(request.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+  if(isLoggedIn !== 'true') {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+      /*
+       * Match all request paths except for the ones starting with:
+       * - api (API routes)
+       * - _next/static (static files)
+       * - _next/image (image optimization files)
+       * - favicon.ico (favicon file)
+       */
+      '/((?!api|_next/static|_next/image|favicon.ico|next.svg|vercel.svg).*)',
+    ],
+  }
